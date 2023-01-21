5610f8f7893ab53b7c6bd910ba12fdf976729ccb
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-21 13:14:59 +0000

    if else will not work

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
+const alert = document.querySelector(".alert");
+const searchValue = document.querySelector(".search_input").value;
+const stringPassIn = `https://ffxivcollect.com/api/minions?name_en_cont=${searchValue}`;
+
 //***FUNCTIONS***
 // search function
-let search = (e) => {
-  const searchValue = document.querySelector(".search_input").value;
-  const stringPassIn = `https://ffxivcollect.com/api/minions?name_en_cont=${searchValue}`;
+function search(e) {
+  console.log(searchValue);
   console.log(stringPassIn);
-  fetch(stringPassIn)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-};
+  if (searchValue != "") {
+    fetch(stringPassIn)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        returnToDefault();
+      });
+  } else {
+    displayError();
+  }
+}
 
+function displayError() {
+  alert.textContent = "Please enter a value";
+  alert.classList.remove("hidden");
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.add("hidden");
+  }, 1000);
+}
+function returnToDefault() {
+  searchValue.textContent = "";
+}
+
+// event listeners
 document.querySelector(".search-button").addEventListener("click", search);
