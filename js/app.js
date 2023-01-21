fbef8e6ed88746a86c6e8ff04bc757dc7c7267cf
Leoni <118344314+lktay@users.noreply.github.com>
2023-01-21 16:19:00 +0000

    added name!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const alert = document.querySelector(".alert");
 const infoContainer = document.querySelector(".container");
 const minionImgContainer = document.querySelector(".img-container");
+const minionNameContainer = document.querySelector(".name-container");
 const descriptionBox = document.querySelector("#nav-description");
 const enDescriptionBox = document.querySelector("#nav-enhanced-description");
 const toolTip = document.querySelector("#nav-tooltip");
@@ -37,6 +38,7 @@ async function search(e) {
         minionImgContainer.classList.remove("hidden");
         minionImgContainer.innerHTML = `<img src="${minionImg}" alt="image of ${minionName}">`;
         infoContainer.classList.remove("hidden");
+        minionNameContainer.innerHTML = `<p>${minionName}</p>`;
         descriptionBox.innerHTML = `<p>${minionDescription}</p>`;
         enDescriptionBox.innerHTML = `<p>${minionEnDescription}</p>`;
         toolTip.innerHTML = `<p>${minionTooltip}</p>`;
@@ -50,6 +52,45 @@ async function search(e) {
   }
 }
 
+//random Search
+
+function randSearch(e) {
+  e.preventDefault();
+  let rand = Math.floor(Math.random() * 472);
+  let stringPassIn = `https://ffxivcollect.com/api/minions/${rand}`;
+  fetch(stringPassIn)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      //defining data name
+      minionData = data;
+      minionName = minionData.name;
+      minionImg = minionData.image;
+      minionDescription = minionData.description;
+      minionEnDescription = minionData.enhanced_description;
+      minionTooltip = minionData.tooltip;
+
+      //showing the info in divs
+      minionImgContainer.classList.remove("hidden");
+      minionImgContainer.innerHTML = `<img src="${minionImg}" alt="image of ${minionName}">`;
+      infoContainer.classList.remove("hidden");
+      minionNameContainer.innerHTML = `<p>${minionName}</p>`;
+      descriptionBox.innerHTML = `<p>${minionDescription}</p>`;
+      enDescriptionBox.innerHTML = `<p>${minionEnDescription}</p>`;
+      toolTip.innerHTML = `<p>${minionTooltip}</p>`;
+      searchValue.value = "";
+      alert.textContent = "";
+      alert.classList.add("hidden");
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+}
+
 //error function
 function displayError() {
   alert.textContent = "Please enter a value";
@@ -58,3 +99,4 @@ function displayError() {
 
 // event listeners
 document.querySelector(".search-button").addEventListener("click", search);
+document.querySelector(".random-button").addEventListener("click", randSearch);
